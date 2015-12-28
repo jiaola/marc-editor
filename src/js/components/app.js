282d6621a2506f7f50e7fd97a48f2f25a4df267c
djiao <djiao@iu.edu>
2015-12-27 22:21:07 -0500

    Remove the ?_k= junk in the URL. See https://github.com/rackt/react-router/blob/master/docs/guides/basics/Histories.md#what-is-that-_kckuvup-junk-in-the-url

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -4,9 +4,16 @@ import Template from './app-template';
 import MarcEditor from './marc-editor';
 import {Router, Route, IndexRoute} from 'react-router';
 
+import createHistory from 'history/lib/createHashHistory';
+
+// Opt-out of persistent state, not recommended.
+var history = createHistory({
+  queryKey: false
+});
+
 export default() => {
   return (
-    <Router>
+    <Router history={history}>
       <Route path="/" component={Template}>
         <IndexRoute component={MarcEditor}/>
         <Route path="upload" component={Uploader}/>
